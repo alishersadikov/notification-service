80df76f3e33fa133471b6134818fe91cfcd31a3f
Alisher Sadikov <alishersadikov@gmail.com>
2020-11-01 20:26:33 -0600

    Routes and controller shells

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,4 @@
+class CallbacksController < ApplicationController 
+  def text
+  end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do 
+    namespace :v1 do 
+      post '/send_text', to: "notifications#create"
+    end 
+  end 
+
+  post '/delivery_status', to: 'callbacks#text'
 end
