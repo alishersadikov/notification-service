c3dc84c99d3d712bdf99710fd250a04b2b080725
Alisher Sadikov <alishersadikov@gmail.com>
2020-11-04 14:05:31 -0600

    Working and tested queue notification service

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 class Api::V1::NotificationsController < ActionController::API
   def create
-    LoadBalancerService.process(
+    QueueNotificationService.process(
       number: notification_params[:number],
       message: notification_params[:message]
     )
+
+    # render json: {
+    #   number: notification_params[:number],
+    #   message: notification_params[:message]
+    # }, status: :accepted
   end
 
   private
--- app/services/queue_notification_service.rb
+++ app/services/queue_notification_service.rb
@@ -4,30 +4,38 @@ class QueueNotificationService
     new(args).process
   end
 
-  def initialize(number:, message:, provider_url:)
+  def initialize(number:, message:)
     @number = number
     @message = message
-    @provider_url = provider_url
   end
 
   def process
+    determine_provider_url
     create_notification
     queue_notification
+  end
+
+  def determine_provider_url
+    @provider_url = LoadBalancerService.process
   end
 
   def create_notification
     @notification = Notification.create!(
       number: @number,
       text: @message,
-      provider_url: provider_url,
+      provider_url: @provider_url
     )
   end
 
   def queue_notification
+    return unless @notification
+
+    ngrok_host = File.open('.ngrok_host').read
+
     params = {
       "to_number": @notification.number,
       "message": @notification.text,
-      "callback_url": "host/delivery_status"
+      "callback_url": "#{ngrok_host}/delivery_status"
     }
 
     HTTParty.post(@notification.provider_url, params: params.to_json)
