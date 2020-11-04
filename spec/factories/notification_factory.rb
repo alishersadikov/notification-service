e932a6a08dfc9ca657f9483447ad3688c0c251a6
Alisher Sadikov <alishersadikov@gmail.com>
2020-11-04 14:05:31 -0600

    Working and tested queue notification service

--- spec/factories/notification_factory.rb
+++ spec/factories/notification_factory.rb
@@ -1,8 +1,7 @@
-
 # frozen_string_literal: true
 FactoryBot.define do
   factory :notification do
-    number { Faker::Number.number(digits: 10) }
+    number { Faker::Number.number(digits: 10).to_s }
     text { Faker::Lorem.sentence(word_count: 3, supplemental: true) }
     trait :provider_1 do
       provider_url { ENV['PROVIDER_1_URL'] }
