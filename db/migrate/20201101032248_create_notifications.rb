19aeaf5fe7b8415f17b63eae3383013fa088f3dc
Alisher Sadikov <alishersadikov@gmail.com>
2020-11-04 14:22:35 -0600

    Notification column name is consistent

--- db/migrate/20201101032248_create_notifications.rb
+++ db/migrate/20201101032248_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[6.0]
   def change
     create_table :notifications do |t|
       t.string :number
-      t.text :text
+      t.text :message
       t.string :status
       t.string :provider_url
       t.uuid :external_id
