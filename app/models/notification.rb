3adeb925c67bcece78874d8be83174b3489a1a18
Alisher Sadikov <alishersadikov@gmail.com>
2020-11-04 20:40:35 -0600

    Notification can have a child

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,4 @@
 class Notification < ApplicationRecord
+  has_one :child, class_name: 'Notification', foreign_key: 'notification_id'
+  belongs_to :parent, class_name: 'Notification', foreign_key: 'notification_id', optional: true
 end
