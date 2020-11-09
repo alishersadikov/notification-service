6c40c5c5cc87f49fe1768cc2be7d7e859ae0b94e
Alisher Sadikov <alishersadikov@gmail.com>
2020-11-09 08:29:08 -0600

    Seed providers

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Provider.create(url: 'https://jo3kcwlvke.execute-api.us-west-2.amazonaws.com/dev/provider1' , weight: 30.0)
+Provider.create(url: 'https://jo3kcwlvke.execute-api.us-west-2.amazonaws.com/dev/provider2' , weight: 70.0)
